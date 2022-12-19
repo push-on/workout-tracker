8e60579e30c6504ec48746ee05e2d2885908138d
mhimranhossain <mh.imranhossain@outlook.com>
2022-12-19 11:58:43 +0600

    workout forms

--- frontend/src/lib/Home.jsx
+++ frontend/src/lib/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { WorkoutDetails } from './WorkoutDetails'
+import { WorkoutForm } from './WorkoutForm'
 
 export const Home = () => {
   const [workouts, setWorkouts] = useState(null)
@@ -8,7 +9,7 @@ export const Home = () => {
     const fetchWorkouts = async () => {
       const response = await fetch('http://localhost:4000/api/workouts')
       const json = await response.json()
-      
+
       if (response.ok) {
         setWorkouts(json)
       }
@@ -17,12 +18,13 @@ export const Home = () => {
   }, [])
 
   return (
-    <div className="">
-      <div className="space-y-5 max-w-md">
+    <div className="flex flex-col justify-between md:flex-row md:space-x-10  ">
+      <div className="max-w-sm space-y-5  grow">
         {workouts && workouts.map(workout => (
           <WorkoutDetails workout={workout} key={workout._id} />
-        ))}
+          ))}
       </div>
+          <WorkoutForm />
     </div>
   )
 }
---
+++ frontend/src/lib/WorkoutForm.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+export const WorkoutForm = () => {
+  const [title, setTitle] = useState('')
+  const [load, setLoad] = useState('')
+  const [reps, setReps] = useState('')
+  const [error, setError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const workout = { title, load, reps }
+    const response = await fetch('http://localhost:4000/api/workouts', {
+      method: `POST`,
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const json = await response.json()
+
+    if (!response.ok) {
+      setError(json.error)
+    }
+    if (response.ok) {
+      setTitle('')
+      setLoad('')
+      setReps('')
+      setError(null)
+      console.log('new workout added')
+    }
+  }
+
+  return (
+
+    <form className='flex flex-col space-y-3 grow max-w-xs pt-10 md:pt-0 ' onSubmit={handleSubmit}>
+      <h3 className='text-blue-400 text-2xl font-bold'>Add a New Workout</h3>
+      <label className='font-bold'>Exercise Title</label>
+      <input
+        className='input-box '
+        type="text"
+        onChange={(e) => { setTitle(e.target.value) }}
+        value={title}
+        placeholder="push-ups"
+      />
+      <label className='font-bold'>Load (in Kg):</label>
+      <input
+        className='input-box'
+        type="number"
+        onChange={(e) => { setLoad(e.target.value) }}
+        value={load}
+        placeholder="10"
+      />
+      <label className='font-bold'>Reps:</label>
+      <input
+        className='input-box '
+        type="number"
+        onChange={(e) => { setReps(e.target.value) }}
+        value={reps}
+        placeholder="5"
+      />
+      <button className='btn'>Add Workout</button>
+      {error && <div className='text-red-400 italic'>{error}</div>}
+    </form>
+  )
+}
